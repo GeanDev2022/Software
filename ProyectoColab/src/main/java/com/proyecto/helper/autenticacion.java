7450a8dbb14c2747a092374b4056bfa7ee914805
GeanDev2022 <96555549+GeanDev2022@users.noreply.github.com>
2022-04-11 21:14:05 -0500

    Se realizan pruebas de conexion

---
+++ ProyectoColab/src/main/java/com/proyecto/helper/autenticacion.java
@@ -0,0 +1,25 @@
+package com.proyecto.helper;
+
+
+
+public class autenticacion {
+	
+	
+	private String emailUsuario;	
+	private String contrasenaUsuario;
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+	public String getContrasenaUsuario() {
+		return contrasenaUsuario;
+	}
+	public void setContrasenaUsuario(String contrasenaUsuario) {
+		this.contrasenaUsuario = contrasenaUsuario;
+	}
+	
+	
+
+}
